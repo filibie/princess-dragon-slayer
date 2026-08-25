c37a1169b6356798a3bdb093582c420069033c96
filibie <filipbiernat@gmail.com>
2026-08-25 22:04:19 +0200

    add player double jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,16 +4,23 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
+var double_jump_counter = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		# Reset double jump counter
+		double_jump_counter = 0
+		
+	# Handle double jump
+	if double_jump_counter < 1 and !is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+		double_jump_counter += 1
 
 	# Get the input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
