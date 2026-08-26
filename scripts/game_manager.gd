e865d1e92f5637654459f3622166c5e581622622
filibie <filipbiernat@gmail.com>
2026-08-26 09:19:19 +0200

    player health points and slime hits

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,14 @@
 extends Node
 
 var score = 0
+var health = 3
 
 @onready var score_label: Label = $ScoreLabel
 
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins."
+	
+func decrease_health():
+	if health > 0:
+		health -= 1
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,13 +1,32 @@
 extends Area2D
 
+@export var is_instant_kill: bool = false
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You died!")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
+	if body.name == "Player" or body.is_in_group("player"):
+		# Skip damage if player is currently rolling
+		if "is_rolling" in body and body.is_rolling:
+			return
+	
+		if is_instant_kill:
+			trigger_death(body)
+		else:
+			print("collision")
+			if GameManager:
+				GameManager.decrease_health()
+				# If health drops to 0 or below, trigger death
+				if GameManager.health <= 0:
+					trigger_death(body)
+			else:
+				print("game manager is null")
 
+func trigger_death(body: Node2D) -> void:
+	print("You died")
+	Engine.time_scale = 0.5
+	if body.has_node("CollisionShape2D"):
+		body.get_node("CollisionShape2D").queue_free()
+	timer.start()
 
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1.0
